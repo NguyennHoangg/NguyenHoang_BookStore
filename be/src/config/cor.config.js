38400abf57630b0f2b5afd6a1eca59275a416e12
Nguyễn Huy Hoàng <139753465+NguyennHoangg@users.noreply.github.com>
2026-09-22 23:29:30 +0700

    Update cor.config.js

--- be/src/config/cor.config.js
+++ be/src/config/cor.config.js
@@ -1,7 +1,7 @@
 const cors = require('cors');
 
 const corsConfig = cors({
-    origin: ['http://localhost:5173', 'https://nguyen-hoang-book-store.vercel.app'],
+    origin: ['http://localhost:5173', 'https://nguyen-hoang-book-store.vercel.app', 'https://nguyen-hoang-book-store-4ufu8ll8r-nguyen-hoang-s-projects.vercel.app/'],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
