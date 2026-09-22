85a21e35626e26f3c1a2f199c5c0288521eaa40c
Nguyễn Huy Hoàng <139753465+NguyennHoangg@users.noreply.github.com>
2026-09-22 23:22:38 +0700

    Revert "fix: add Vercel domain to CORS origin"

--- be/src/config/cor.config.js
+++ be/src/config/cor.config.js
@@ -1,7 +1,7 @@
 const cors = require('cors');
 
 const corsConfig = cors({
-    origin: ['http://localhost:5173', 'https://nguyen-hoang-book-store.vercel.app'],
+    origin: 'http://localhost:5173',
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
